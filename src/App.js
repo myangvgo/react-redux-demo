faaf5b6a383f6810ec5958f7db028b3bd3004a1e
Min Yang <myangvgo@hotmail.com>
2019-11-07 17:43:28 +0800

    feat: add basic redux to react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createStore } from 'redux';
+import Counter from './Counter';
 import './App.css';
 
+const initialState = {
+    count: 0
+};
+
+function reducer(state = initialState, action) {
+    console.log('reducer', state, action);
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1 };
+        case 'DECREMENT':
+            return { count: state.count - 1 };
+        case 'RESET':
+            return { count: 0 };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'RESET' });
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Counter />
+        </div>
+    );
 }
 
 export default App;
